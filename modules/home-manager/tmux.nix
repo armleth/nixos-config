bafe4c7973135c2156facaba5522fd5c7bfeecc8
Armand Thibaudon <armand.thibaudon@epita.fr>
2025-01-28 18:22:13 +0100

    feat: tmux config improvments

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -1,10 +1,18 @@
 { config, ... }:
 
-programs.tmux = {
-    enable = true;
-    clock24 = true;
-    defaultKeyMode = "vi";
-    baseIndex = 1;
-    prefix = 'C-s';
-}
+{
+    programs.tmux = {
+        enable = true;
+        clock24 = true;
+        defaultKeyMode = "vi";
+        baseIndex = 1;
+        escapeTime = 0;
+        prefix = "C-s";
+        historyLimit = 50000;
 
+        extraConfig = ''
+            bind C-p previous-window
+            bind C-n next-window
+        '';
+    };
+}
