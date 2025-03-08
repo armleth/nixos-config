de84a6ffaad19e54662a5a99f3ce61bfe685a00f
Armleth <armand.thibaudon@epita.fr>
2025-03-08 23:47:58 +0100

    feat: firefox as default browser

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -60,6 +60,18 @@
       ]);
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+        "default-web-browser"           = [ "firefox.desktop" ];
+        "text/html"                     = [ "firefox.desktop" ];
+        "x-scheme-handler/http"         = [ "firefox.desktop" ];
+        "x-scheme-handler/https"        = [ "firefox.desktop" ];
+        "x-scheme-handler/about"        = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown"      = [ "firefox.desktop" ];
+    };
+  };
+
   programs = {
     java = {
         package = pkgs.jdk17_headless;
